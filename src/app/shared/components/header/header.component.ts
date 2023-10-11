87ebd8e08d533cdab3a2310da0ca937618c4fe62
adus0205 <adiszu89@gmail.com>
2023-10-11 21:22:50 +0200

    dodanie aktywnej ikony koszyka

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { HeaderService } from './header.service';
+import { count } from 'rxjs';
+import { CartIconService } from 'src/app/modules/common/service/cart-icon.service';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +12,22 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title = "Shop";
+  cartProductCounter = "";
 
-  constructor(){}
+  constructor(
+    private cookieService: CookieService,
+    private headerService: HeaderService,
+    private cartItemService: CartIconService){}
 
   ngOnInit(): void{
+    this.getCountProducts();
+    this.cartItemService.subject
+      .subscribe(countr => this.cartProductCounter = String(countr>0 ? countr:""));
+  }
+
+  getCountProducts(){
+    this.headerService.getCountProducts(Number(this.cookieService.get("cartId")))
+      .subscribe(countr => this.cartProductCounter = String(countr > 0 ? countr:""));
   }
 
 }
