9cbf10fef03ca79630a4686c5acab8dcab3697e0
adus0205 <adiszu89@gmail.com>
2024-01-26 16:56:03 +0100

    Dodanie statusu zamówień dla profilu klienta

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from './profile.service';
+import { OrderListDto } from './model/orderListDto';
+import { JwtPayload } from 'jwt-decode';
+import { JwtService } from '../common/service/jwt.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  orders!: Array<OrderListDto>
+  displayedColumns = ["id", "placeDate", "orderStatus", "grossValue"];
+
+  constructor(private profileService: ProfileService,
+    private jwtService: JwtService,
+    private router: Router) {}
+
+  ngOnInit(): void {
+    if(!this.jwtService.isLoggedIn()){
+      this.router.navigate(["/login"]);
+    }
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.profileService.getOrders()
+      .subscribe(orders => this.orders = orders);
+  }
 
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { CookieService } from 'ngx-cookie-service';
 import { HeaderService } from './header.service';
 import { count } from 'rxjs';
 import { CartIconService } from 'src/app/modules/common/service/cart-icon.service';
+import { JwtService } from 'src/app/modules/common/service/jwt.service';
 
 @Component({
   selector: 'app-header',
@@ -13,16 +14,19 @@ export class HeaderComponent implements OnInit {
 
   title = "Shop";
   cartProductCounter = "";
+  isLoggedIn = false;
 
   constructor(
     private cookieService: CookieService,
     private headerService: HeaderService,
-    private cartItemService: CartIconService){}
+    private cartItemService: CartIconService,
+    private jwtService: JwtService){}
 
   ngOnInit(): void{
     this.getCountProducts();
     this.cartItemService.subject
-      .subscribe(countr => this.cartProductCounter = String(countr>0 ? countr:""));
+      .subscribe(countr => this.cartProductCounter = String(countr >0 ? countr:""));
+    this.isLoggedIn = this.jwtService.isLoggedIn();  
   }
 
   getCountProducts(){
