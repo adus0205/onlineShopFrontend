8192ec5f5d8dcc391d3d9bf8eca36bd3e7452003
adus0205 <adiszu89@gmail.com>
2024-02-26 16:54:27 +0100

    migracja do nowej wersji angulara

--- src/app/modules/common/guard/profileAuthorizedGuard.ts
+++ src/app/modules/common/guard/profileAuthorizedGuard.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { JwtService } from "src/app/modules/common/service/jwt.service";
 
 @Injectable()
-export class ProfileAuthorizeGuard implements CanActivate {
+export class ProfileAuthorizeGuard  {
 
     constructor(private jwtService: JwtService, private router: Router){}
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
